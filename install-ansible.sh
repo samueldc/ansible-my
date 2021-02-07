2fa168374ed9c6733f9830849697919b435d2548
samueldc <nedmaj@yahoo.com>
2021-02-07 19:36:39 -0300

    Begining with playbooks

--- install-ansible.sh
+++ install-ansible.sh
@@ -3,3 +3,14 @@ apt update
 apt install software-properties-common
 apt-add-repository --yes --update ppa:ansible/ansible
 apt install ansible
+
+# Argcomplete
+apt install python-argcomplete
+activate-global-python-argcomplete
+
+# Inventory
+printf "[local]\n127.0.0.1 ansible_connection=local" | tee -a /etc/ansible/hosts
+
+# Test Ansible
+ansible all -m ping
+ansible all -a "/bin/echo hello" --become
