ed5cbfd09b1ecca1dff9e6fe78eb4bed531ccacc
samueldc <nedmaj@yahoo.com>
2021-03-01 21:24:45 -0300

    Variables implementation

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,29 +1,63 @@
 #!/bin/bash
+
+# usage
+usage() {
+   cat << EOF
+Usage: sudo install-ansible.sh -u <username>
+EOF
+   exit 1
+}
+
+# verify if user is root
 if [ "$(id -u)" -ne 0 ]; then
    echo "Error: Use sudo to run this script."
    exit 1
 fi
 
+# verify parameters
+if [ $# -ne 2 ]; then
+   usage
+fi
+
+# get options
+while getopts ":u:" opt
+do
+   case $opt in
+      u) 
+         username=$OPTARG;;
+      \?)
+         echo "Invalid option: $OPTARG"
+         exit 1;;
+      :)
+         echo "Option -$opt needs a argument"
+         exit;;
+   esac
+done
+
+# install ansible
+#apt-add-repository --yes --update ppa:ansible/ansible
 apt update
 apt install software-properties-common
-#apt-add-repository --yes --update ppa:ansible/ansible
 apt install ansible
 
-# Argcomplete
-apt install python3-argcomplete
-activate-global-python-argcomplete3
-
-# Inventory
-printf "[local]\n127.0.0.1 ansible_connection=local" | tee -a /etc/ansible/hosts
-printf "\n[local:vars]\nansible_python_interpreter=/usr/bin/python3" | tee -a /etc/ansible/hosts
-
-# Test Ansible
-ansible all -m ping
-ansible all -a "/bin/echo hello" --become
-
-# Install Ansible modules
+# install ansible modules
 ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.general
 
-# Run Ansible
+# install python argmcomplete
+apt install python3-argcomplete
+activate-global-python-argcomplete3
+
+# inventory local machine
+printf "\n[local]" | tee -a /etc/ansible/hosts
+printf "\n127.0.0.1 ansible_connection=local" | tee -a /etc/ansible/hosts
+printf "\n[local:vars]" | tee -a /etc/ansible/hosts
+printf "\nansible_python_interpreter=/usr/bin/python3" | tee -a /etc/ansible/hosts
+printf "\nusername=$username" | tee -a /etc/ansible/hosts
+
+# test ansible
+ansible all -m ping
+ansible all -a "/bin/echo hello" --become
+
+# run ansible playbook
 ansible-playbook local.yml --ask-become-pass
